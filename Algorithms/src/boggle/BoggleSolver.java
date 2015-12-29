1c0b99455b98e671c886dfe3a17b32891d117e42
lixiao <shutianhe@gmail.com>
2015-12-30 04:13:38 +0800

    promote performance

--- Algorithms/src/boggle/BoggleSolver.java
+++ Algorithms/src/boggle/BoggleSolver.java
@@ -1,18 +1,20 @@
 package boggle;
 
+
+
+
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.TrieSET;
 
 public class BoggleSolver {
 	private TrieTree dictionary;
 	private int height, width;
 
-	// Initializes the data structure using the given array of strings as the dictionary.
+	// Initializes the data structure using the given array of strings as the
+	// dictionary.
 	public BoggleSolver(String[] dictionary) {
 		this.dictionary = new TrieTree();
 		for (String word : dictionary) {
@@ -20,8 +22,9 @@ public class BoggleSolver {
 		}
 		System.out.println("finish dictionary");
 	}
- 
-	// Returns the set of all valid words in the given Boggle board, as an Iterable.
+
+	// Returns the set of all valid words in the given Boggle board, as an
+	// Iterable.
 	public Iterable<String> getAllValidWords(BoggleBoard board) {
 		this.width = board.cols();
 		this.height = board.rows();
@@ -34,22 +37,14 @@ public class BoggleSolver {
 				getPath(board, results, path, i, j, marked);
 			}
 		}
-		// test
-		// ArrayList<Character> path = new ArrayList<>();
-		// path.add('E');
-		// boolean[][] marked = new boolean[height][width];
-		// getPath(board, results, path, 3, 3, marked);
-
-		ArrayList<String> sort = new ArrayList<>();
-		sort.addAll(results);
-		Collections.sort(sort);
-		return sort;
+		return results;
 	}
 
 	// Returns the score of the given word if it is in the dictionary, zero
 	// otherwise.
 	public int scoreOf(String word) {
-		if (!dictionary.contains(word)) return 0;
+		if (!dictionary.contains(word))
+			return 0;
 		int length = word.length();
 		if (length < 3)
 			return 0;
@@ -68,44 +63,38 @@ public class BoggleSolver {
 	private void getPath(BoggleBoard board, HashSet<String> results, ArrayList<Character> path, int row, int col,
 			boolean[][] marked) {
 		marked[row][col] = true;
-		// System.out.println(path);
-		// if (path.size() == 4 && path.get(0) == 'E' && path.get(1) == 'N' &&
-		// path.get(2) == 'I' && path.get(3) == 'E') {
-		// System.out.println(path);
-		// }
 		String string = charToString(path);
-		if (!prefixQuery(string))
+		if (!prefixQuery(path))
 			return;
+//		if (!prefixQuery(string))
+//			return;
 		if (!results.contains(string)) {
 			// System.out.println(string);
 			if (this.dictionary.contains(string) && string.length() > 2) {
 				results.add(string);
 			}
 		}
-		ArrayList<Integer[]> neighbors = (ArrayList<Integer[]>) getNeighbors(board, row, col);
-		// for(Integer[] xy:neighbors) {
-		// System.out.print(board.getLetter(xy[0], xy[1]) + "\t" );
-		// }
-		// System.out.println();
+		ArrayList<Integer[]> neighbors = (ArrayList<Integer[]>) getNeighbors(board, row, col, marked);
 		for (Integer[] neighbor : neighbors) {
 			int x = neighbor[0];
 			int y = neighbor[1];
-			if (!marked[x][y]) {
-				Character c = board.getLetter(x, y);
-				path.add(c);
-				getPath(board, results, path, x, y, marked);
-				path.remove(path.size() - 1);
-				marked[x][y] = false;
-			}
+			Character c = board.getLetter(x, y);
+			path.add(c);
+			getPath(board, results, path, x, y, marked);
+			path.remove(path.size() - 1);
+			marked[x][y] = false;
 		}
 	}
 
+	private boolean prefixQuery(ArrayList<Character> query) {
+		return this.dictionary.hasPrefix(query);
+	}
 	private boolean prefixQuery(String query) {
 		return this.dictionary.hasPrefix(query);
 	}
 
 	private String charToString(ArrayList<Character> path) {
-		StringBuffer result = new StringBuffer();
+		StringBuilder result = new StringBuilder();
 		for (Character c : path) {
 			if (c == 'Q') {
 				result.append("QU");
@@ -116,50 +105,49 @@ public class BoggleSolver {
 		return result.toString();
 	}
 
-	private Iterable<Integer[]> getNeighbors(BoggleBoard board, int row, int col) {
+	private Iterable<Integer[]> getNeighbors(BoggleBoard board, int row, int col, boolean[][] marked) {
 		ArrayList<Integer[]> neighbors = new ArrayList<>();
 		// up
-		if (row > 0) {
+		if (row > 0 && !marked[row - 1][col]) {
 			Integer[] n = { row - 1, col };
 			neighbors.add(n);
 		}
 		// down
-		if (row < height - 1) {
+		if (row < height - 1 && !marked[row + 1][col]) {
 			Integer[] n = { row + 1, col };
 			neighbors.add(n);
 		}
 		// left
-		if (col > 0) {
+		if (col > 0 && !marked[row][col - 1]) {
 			Integer[] n = { row, col - 1 };
 			neighbors.add(n);
 		}
 		// right
-		if (col < width - 1) {
+		if (col < width - 1 && !marked[row][col + 1]) {
 			Integer[] n = { row, col + 1 };
 			neighbors.add(n);
 		}
 		// up and left
-		if (row > 0 && col > 0) {
+		if (row > 0 && col > 0 && !marked[row - 1][col - 1]) {
 			Integer[] n = { row - 1, col - 1 };
 			neighbors.add(n);
 		}
 		// up and right
-		if (row > 0 && col < width - 1) {
+		if (row > 0 && col < width - 1 && !marked[row - 1][col + 1]) {
 			Integer[] n = { row - 1, col + 1 };
 			neighbors.add(n);
 		}
 		// down and left
-		if (row < height - 1 && col > 0) {
+		if (row < height - 1 && col > 0 && !marked[row + 1][col - 1]) {
 			Integer[] n = { row + 1, col - 1 };
 			neighbors.add(n);
 		}
 		// down and right
-		if (row < height - 1 && col < width - 1) {
+		if (row < height - 1 && col < width - 1 && !marked[row + 1][col + 1]) {
 			Integer[] n = { row + 1, col + 1 };
 			neighbors.add(n);
 		}
 		return neighbors;
-
 	}
 
 	public static void main(String[] args) {
